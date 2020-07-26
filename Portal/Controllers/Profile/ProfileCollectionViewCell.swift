cec538682fc2874e6f6e3ee6ec3bb3e3e7b91c59
evanrenanto <62125175+evanrenanto@users.noreply.github.com>
2020-07-26 16:33:00 +0700

    profile page progres done, + benerin percantil tampil bbrp page lainnya

---
+++ Portal/Controllers/Profile/ProfileCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileCollectionViewCell.swift
+//  Portal
+//
+//  Created by Evan Renanto on 26/07/20.
+//  Copyright © 2020 Dedy Yuristiawan. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ImageBackgroundStandUpComedian: UIImageView!
+    @IBOutlet weak var CaptionStandUp: UILabel!
+    @IBOutlet weak var AudienceCount: UILabel!
+    @IBOutlet weak var ViewerLogo: UIImageView!
+    
+}
